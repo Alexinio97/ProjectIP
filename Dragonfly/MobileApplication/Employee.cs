091b736a1f79cf51d172c4fff3c2cca710a77b34
Alexinio97 <alex.andricsak@gmail.com>
2019-04-04 17:48:56 +0300

    Added two prototype classes

---
+++ Dragonfly/MobileApplication/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileApplication
+{
+    class Employee
+    {
+        private int _accessCode;
+        private string _badge;
+        private string _firstName;
+        private string _lastName;
+
+        public Employee(int accessCode, string badge, string firstName, string lastName)
+        {
+            _accessCode = accessCode;
+            _badge = badge;
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public int AccessCode { get => _accessCode; set => _accessCode = value; }
+        public string Badge { get => _badge; set => _badge = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+    }
+}
---
+++ Dragonfly/MobileApplication/WorkingHours.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileApplication
+{
+    class WorkingHours : Employee
+    {
+        private DateTime startTime;
+        private DateTime exitTime;
+        private DateTime workingDay;
+        private bool checkIn;
+
+        public WorkingHours(DateTime startTime, DateTime exitTime, DateTime workingDay, bool checkIn, int accessCode, string badge, string firstName, string lastName) : 
+            base(accessCode,badge,firstName,lastName)
+        {
+            this.startTime = startTime;
+            this.exitTime = exitTime;
+            this.workingDay = workingDay;
+            this.checkIn = checkIn;
+        }
+
+        // setters and getters
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime ExitTime { get => exitTime; set => exitTime = value; }
+        public DateTime WorkingDay { get => workingDay; set => workingDay = value; }
+        public bool CheckIn { get => checkIn; set => checkIn = value; }
+    }
+}
